920a9c95677098d7a381da36ace1f5923cb99962
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-06-11 13:17:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let nameIn=document.getElementById("username").value;
+let passIn=document.getElementById("password").value;
+let submit=document.getElementById("submit").value;
+let checkBox=document.getElementById("checkbox").checked;
