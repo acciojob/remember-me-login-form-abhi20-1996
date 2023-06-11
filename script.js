8392a07809e2e25fe25ebf6b72cbbfefa19c4d33
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-06-11 14:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 //your JS code here. If required.
 let nameIn=document.getElementById("username").value;
 let passIn=document.getElementById("password").value;
-let submit=document.getElementById("submit").value;
-let checkBox=document.getElementById("checkbox").checked;
+let submit=document.getElementById("submit");
+let check=document.getElementById("checkbox").checked;
+let btn=document.getElementById("existing");
+submit.addEventListener('click',(e)=>{
+	e.preventDefault();
+	if (check) {
+		localStorage.setItem("username", JASON.stringify(nameIn));
+		localStorage.setItem("password", JASON.stringify(passIn));
+		btn.style.display="block";
+	}
+	alert(`Logged in as ${nameIn}`);
+	
+});
+btn.addEventListener('click',()=>{
+	if (check) {
+		alert("Logged in as "+ localStorage.username);
+	}
+})
