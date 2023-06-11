8700552a3ff7ef3299f24283759667b43a07c9a1
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-06-11 15:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-//your JS code here. If required.
-let username = document.getElementById("username").value;
-let pass = document.getElementById("password").value;
-let check = document.getElementById("checkbox").checked;
+document.getElementById("submit").addEventListener('click', function(e) {
+  e.preventDefault();
+  let username = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  let check = document.getElementById("checkbox").checked;
 
+  if (check) {
+    localStorage.setItem("name", JSON.stringify(username));
+    localStorage.setItem("password", JSON.stringify(pass));
+    document.getElementById("existing").style.display = "block";
+  } else {
+    localStorage.removeItem("name");
+    localStorage.removeItem("password");
+  }
+  alert(`Logged in as ${username}`);
+});
 
-function exisFunc(){
-	let Name = JSON.parse(localStorage.getItem("name"));
-	alert(`Logged in as ${Name}`);
+document.getElementById("existing").addEventListener('click', function() {
+  let Name = JSON.parse(localStorage.getItem("name"));
+  alert(`Logged in as ${Name}`);
+});
+
+if (localStorage.getItem("name") && localStorage.getItem("password")) {
+  document.getElementById("existing").style.display = "block";
 }
-
-function myFunc(e){
-	e.preventDefault();
-	if(check){
-		localStorage.setItem("name",JSON.stringify(username));
-		localStorage.setItem("password",JSON.stringify(pass));
-		document.getElementById("existing").style.display = "block";
-		
-	}
-		alert(`Logged in as ${username}`);
-}
-
-document.getElementById("submit").addEventListener('click',myFunc);
-document.getElementById("existing").addEventListener('click',exisFunc);
-if(localStorage.getItem("username") && localStorage.getItem("password")){
-	document.getElementById("existing").style.display = "block";
-}
-
-
-
-
